ec4ce6cb149f5ed0c3a9e8d7dc609e7d5831664c
Samuel <samuel.schuetz6@gmail.com>
2026-08-27 21:52:55 -0600

    Consolidate sorting.py into the correct deliverables folder

--- projects or assignments i turn in live here/sorting.py
+++ projects or assignments i turn in live here/sorting.py
@@ -1,9 +1,19 @@
-# Name:
+# Name: Samuel Schuetz
 # Course: Advanced Algorithms
-# Date:
+# Date: 8/27/2026 - 9/03/2027
 
 import random
 import time
 
 import pandas as pd
 import pytest
+
+
+# python refresher
+
+nums = [1,2,3,4,5,6,7,8,9]
+
+sub_of_nums = nums[1:5]  # ends at '6'
+
+print(sub_of_nums)
+
